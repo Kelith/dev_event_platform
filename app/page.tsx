36623711b08c0e004d50e1e5927a3be9dd729570
Davide Garofalo <davide.garofalo@estiasolutions.it>
2026-01-26 09:26:10 +0100

    refactored folders

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Hello from "../components/hello";
+
+const Page = () => {
+
+    console.log("bibidibobidi")
+
+    return (
+        <div>
+            <Hello/>
+            Page
+        </div>
+    )
+}
+export default Page
---
+++ components/hello.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+'use client'
+
+const Hello = () => {
+    return (
+        <div>Hello</div>
+    )
+}
+export default Hello
