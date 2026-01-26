54ce683d24c054ae60c08b3a33a9db3922d80da8
Davide Garofalo <davide.garofalo@estiasolutions.it>
2026-01-26 09:36:06 +0100

    Revert "refactored folders"
    This reverts commit 36623711b08c0e004d50e1e5927a3be9dd729570.

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,8 @@
-import type {NextConfig} from "next";
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    reactCompiler: true,
-    experimental: {
-        turbopackFileSystemCacheForDev: true
-    }
+  /* config options here */
+  reactCompiler: true,
 };
 
 export default nextConfig;
